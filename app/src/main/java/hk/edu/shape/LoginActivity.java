c9b86931918c4a1f332d820a0ecb927a568a540d
hinson23701 <104848707+hinson23701@users.noreply.github.com>
2024-01-05 12:20:29 +0800

    Login Function Completed

--- app/src/main/java/hk/edu/shape/LoginActivity.java
+++ app/src/main/java/hk/edu/shape/LoginActivity.java
@@ -1,10 +1,26 @@
 package hk.edu.shape;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,15 +28,57 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
+    private EditText email, password;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.editTextUsername);
+        password = findViewById(R.id.editTextPassword);
     }
+
+    public void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Intent intent = new Intent(LoginActivity.this, MapActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
 
     public void RegisterClick(View view) {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
+
+    }
+    private void updateUI(FirebaseUser user) {
+
+    }
+
+
+    public void Login(View view) {
+        String Signinemail = email.getText().toString().trim();
+        String SigninPass = password.getText().toString().trim();
+        signIn(Signinemail, SigninPass);
     }
 }
 
