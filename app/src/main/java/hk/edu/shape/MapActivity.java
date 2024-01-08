45e00e37ea9e372d0dcd28a5b72b0a98e78ae613
hinson23701 <104848707+hinson23701@users.noreply.github.com>
2024-01-08 13:58:41 +0800

    Location function and UI design of hong kong place updated

--- app/src/main/java/hk/edu/shape/MapActivity.java
+++ app/src/main/java/hk/edu/shape/MapActivity.java
@@ -5,9 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -15,45 +25,98 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-// Implement OnMapReadyCallback.
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
     private GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_maps);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Check and request location permissions
+        checkLocationPermission();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     *
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            getLastLocation();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        String fineLocationPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+        String coarseLocationPermission = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), fineLocationPermission) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), coarseLocationPermission) == PackageManager.PERMISSION_GRANTED) {
+                fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            Location location = task.getResult();
+                            double latitude = location.getLatitude();
+                            double longitude = location.getLongitude();
+                            // Do something with the latitude and longitude values
+                        } else {
+                            // Failed to retrieve location
+                        }
+                    }
+                });
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{coarseLocationPermission}, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{fineLocationPermission}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                Toast.makeText(this, "Location permission denied.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in mall and move the camera
+        // Add a marker in Vcity and move the camera
         LatLng Vcity = new LatLng(22.395963137448877, 113.97410476135805);
         mMap.addMarker(new MarkerOptions()
                 .position(Vcity)
                 .title("Vcity"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Vcity));
-        // Add a marker in mall and move the camera
+
+        // Add a marker in Tuen Mun Plaza and move the camera
         LatLng TMPlaza = new LatLng(22.393701452383166, 113.97646510503148);
         mMap.addMarker(new MarkerOptions()
                 .position(TMPlaza)
                 .title("Tuen Mun Plaza"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(TMPlaza));
+    }
+
+    public void PlaceClick(View view) {
+        Intent intent = new Intent(this, PlaceActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/hk/edu/shape/PlaceActivity.java
@@ -0,0 +1,15 @@
+package hk.edu.shape;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class PlaceActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_place);
+
+
+
+    }
+}
