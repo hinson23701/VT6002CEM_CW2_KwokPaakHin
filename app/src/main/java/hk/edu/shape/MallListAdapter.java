2fce0c1d0fed0c33f6beb0f25c86df1948bc70a7
hinson23701 <104848707+hinson23701@users.noreply.github.com>
2024-01-09 16:13:07 +0800

    Hong Kong Place and local database listview updated

---
+++ app/src/main/java/hk/edu/shape/MallListAdapter.java
@@ -0,0 +1,52 @@
+package hk.edu.shape;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class MallListAdapter extends ArrayAdapter<Place> {
+    private Context mContext;
+    private List<Place> malls;
+
+    public MallListAdapter(Context context, List<Place> malls) {
+        super(context, 0, malls);
+        mContext = context;
+        this.malls = malls;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false);
+            holder = new ViewHolder();
+            holder.nameTextView = convertView.findViewById(R.id.textViewName);
+            holder.descriptionTextView = convertView.findViewById(R.id.textViewDescription);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Place place = malls.get(position);
+        if (place != null) {
+            holder.nameTextView.setText(place.getName());
+            holder.descriptionTextView.setText(place.getDescription());
+        }
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView nameTextView;
+        TextView descriptionTextView;
+    }
+}
--- app/src/main/java/hk/edu/shape/PlaceActivity.java
+++ app/src/main/java/hk/edu/shape/PlaceActivity.java
@@ -3,13 +3,26 @@ package hk.edu.shape;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class PlaceActivity extends Activity {
 
-
+    private ListView listView;
+    private MallListAdapter adapter;
+    private MallDatabaseHelper databaseHelper;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_place);
+        listView = findViewById(R.id.listview);
+        databaseHelper = new MallDatabaseHelper(this);
+
+        databaseHelper.insertMall(new Place("V city", "V city hosts 130 brands and restaurants in a convenient location adjacent to MTR Tuen Mun Station and Tuen Mun Light Rail Station. The mall’s Bella Beauty Zone features over 20 shops, and foodies will enjoy the first floor Food Spot, where cuisines include Chinese, Japanese, Western and more"));
+
+        List<Place> malls = databaseHelper.getAllMalls();
+        adapter = new MallListAdapter(this, malls);
+        listView.setAdapter(adapter);
 
 
 
