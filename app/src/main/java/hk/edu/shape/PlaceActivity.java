c68b11cf9c1cb57faee476b2436a298d55c9653c
hinson23701 <104848707+hinson23701@users.noreply.github.com>
2024-01-14 12:55:08 +0800

    Add new Mall Data

--- app/src/main/java/hk/edu/shape/PlaceActivity.java
+++ app/src/main/java/hk/edu/shape/PlaceActivity.java
@@ -25,6 +25,17 @@ public class PlaceActivity extends Activity {
             // Add the data to the database if it does not exist
             databaseHelper.insertMall(new Place("V city", "V city hosts 130 brands and restaurants in a convenient location adjacent to MTR Tuen Mun Station and Tuen Mun Light Rail Station. The mall’s Bella Beauty Zone features over 20 shops, and foodies will enjoy the first floor Food Spot, where cuisines include Chinese, Japanese, Western and more"));
             databaseHelper.insertMall(new Place("TM Plaza", "One of the largest shopping malls in the Northwest New Territories with 1000000 square feet of retail space and 400 shops of all varieties."));
+            databaseHelper.insertMall(new Place("Go Go Mall", " The Go Go Mall is the 1/F commercial space above the Eldo Court Shopping Centre along Tuen Mun Heung Sze Wui Road"));
+            databaseHelper.insertMall(new Place("H.A.N.D.S", " Address\n" +
+                    "\n" +
+                    ": 2A Tuen Mun Heung Sze Wui Road, Tuen Mun\n" +
+                    "\n" +
+                    "Opening Hour\n" +
+                    "\n" +
+                    ": 6am-12am"));
+            databaseHelper.insertMall(new Place("Citywalk", " Modern retail complex with 200 stores & restaurants, plus a movie theatre & a park-like courtyard."
+                    ));
+            databaseHelper.insertMall(new Place("D Park", "Sprawling, family-friendly shopping center with 140 stores, eateries & entertainment facilities."));
         }
 
         List<Place> malls = databaseHelper.getAllMalls();
